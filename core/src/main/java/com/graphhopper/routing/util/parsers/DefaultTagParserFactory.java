00797ee125318c34b0b233d931e086b3676630a7
GBPeters <gijstebrij@gmail.com>
2021-08-18 10:00:20 +0200

    commit before merge

--- core/src/main/java/com/graphhopper/routing/util/parsers/DefaultTagParserFactory.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/DefaultTagParserFactory.java
@@ -70,6 +70,8 @@ public class DefaultTagParserFactory implements TagParserFactory {
             return new OSMHikeRatingParser();
         else if (name.equals(HorseRating.KEY))
             return new OSMHorseRatingParser();
+        else if (name.equals(Scenic.KEY))
+            return new OSMScenicParser();
         else if (name.equals(Country.KEY))
             throw new IllegalArgumentException("The property spatial_rules.borders_directory is required in the configuration " +
                     "when using 'country' in encoded_values");
